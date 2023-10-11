786239b17c2d0b038a5606ca785b8d8e8c727385
SkippyWalnuts <beccaearly89@gmail.com>
2023-10-11 14:57:44 +0100

    Added space to Footer HTML

--- src/Footer.js
+++ src/Footer.js
@@ -15,7 +15,7 @@ export default function Footer() {
         >
           Source-Code
         </a>{" "}
-        by <a href="https://visionary-dragon-63ebd9.netlify.app/"
+        by{" "} <a href="https://visionary-dragon-63ebd9.netlify.app/"
          target="_blank"
          rel="true noopener noreferrer">
           Becca Early
